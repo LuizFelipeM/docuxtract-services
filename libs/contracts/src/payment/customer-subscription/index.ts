900476ce28e4b022a0a781cd97f043c0af1f8ca4
Luiz Moura <muniz.mourafelipe@gmail.com>
2025-01-04 22:25:27 -0300

    Removed unnecessary customer subscription events

--- libs/contracts/src/payment/customer-subscription/index.ts
+++ libs/contracts/src/payment/customer-subscription/index.ts
@@ -7,11 +7,6 @@ export enum CustomerSubscriptionEvents {
   created = 'created',
   deleted = 'deleted',
   updated = 'updated',
-  paused = 'paused',
-  resumed = 'resumed',
-  trialWillEnd = 'trial_will_end',
-  pendingUpdateApplied = 'pending_update_applied',
-  pendingUpdateExpired = 'pending_update_expired',
 }
 
 export type CreatedCustomerSubscriptionEvent = Event<
