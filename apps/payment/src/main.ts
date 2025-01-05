807052b9e86d20ef9956343917561e849db142c0
Luiz Moura <muniz.mourafelipe@gmail.com>
2025-01-04 22:20:09 -0300

    Add ValidationPipe to all microservices

--- apps/payment/src/main.ts
+++ apps/payment/src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { PaymentModule } from './payment.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentModule, { rawBody: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
